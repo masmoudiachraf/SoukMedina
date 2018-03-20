217079ec84afc0c88c0f17ba9c9a8c550bd12a5b
masmo <masmo@172.16.7.17>
2018-03-20 13:30:56 +0100

    first Commit

---
+++ src/com/esprit/service/Iservicearticles.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.esprit.service;
+
+import com.esprit.entite.articles;
+import java.util.List;
+
+/**
+ *
+ * @author Djoo
+ */
+public interface Iservicearticles {
+    void insert(articles p);
+    void delete(int id);
+     void update(articles p,int id);
+    List<articles> displayall();
+}
